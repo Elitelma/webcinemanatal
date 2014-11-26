ac162dfb00efac11e9ec09731737748b014dfccc
Gabriel Tibúrcio <tibuurcio@gmail.com>
2014-11-26 20:35:08 -0300

    images

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ var PARSE_CLIENTKEY = "dMsW9PbJ0IXIgr1G6SQTAe3RsEcJyU6JJk1NdAGb";
 
 Parse.initialize(PARSE_ID, PARSE_CLIENTKEY);
 
-//var text = document.getElementById("p1");
-//text.innerHTML = "Mudando texto";
-
 var Natalshopping = Parse.Object.extend("Natalshopping");
 var query = new Parse.Query(Natalshopping);
 query.find({
@@ -13,7 +10,8 @@ query.find({
 		alert("Pegou com sucesso " + films.length + " filmes.");
 		var text = "", i;
 		for(i = 0; i < films.length; i++) {
-			text = text + films[i].get("name") + "      " + films[i].get("schedule") + 
+			text = text + "<img src=" + films[i].get("image").url() + "/>" + 
+			films[i].get("name") + "      " + films[i].get("schedule") + 
 			"      " + films[i].get("shopping") + "      " + films[i].get("gender") + "      " +
 			films[i].get("censure") + "<br><br>";
 		}
